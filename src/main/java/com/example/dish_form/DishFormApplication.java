2b232a8b63be62293cdafc00564505aff00d8f12
verabradu <verabradu92@gmail.com>
2025-06-16 09:12:58 -0500

    Initial commit

---
+++ src/main/java/com/example/dish_form/DishFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.dish_form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dish_form/DishFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dish_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
